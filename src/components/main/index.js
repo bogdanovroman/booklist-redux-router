d3027a516f9fbc56f3506dcc6951758337f4f32d
Roman Bogdanov <rbogdanov@MacBook-Pro-Roman.local>
2017-06-28 09:48:31 +0300

    classnames

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Header from '../header';
+import className from 'classnames';
 
 class Main extends Component {
     render() {
+        const {isLoading} = this.props.utils;
+        let loadingClass = className({loader: true, 'active': !isLoading})
         return (
             <div className="content">
+                <div className={loadingClass}></div>
                 <Header/>
                 <div></div>
             </div>
@@ -14,7 +18,7 @@ class Main extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return {};
+    return {utils: state.utils};
 };
 
 const mapDispatchToProps = (dispatch) => {
